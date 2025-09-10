e1c6257d09577aa482c5b3355f369e94610f4cbc
esteghlal1402 <202157739+esteghlal1402@users.noreply.github.com>
2025-09-10 12:43:31 +0330

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# AhmadCalculator - ماشین‌حساب ساده
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("جمع:", add(5, 3))
+    print("تفریق:", subtract(10, 4))
